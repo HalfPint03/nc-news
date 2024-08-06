17e0d68a1af069c5d49aad337b634577ddf9ae2f
Ashlyn Lovatt <ashlynlovatt@gmail.com>
2024-08-06 16:15:47 +0100

    task 6 comments

--- Components/Utils/SingleArticleCard.jsx
+++ Components/Utils/SingleArticleCard.jsx
@@ -1,4 +1,11 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import formatDate from "./formatDate";
+import Comments from "../Comments";
+
 function SingleArticleCard(article) {
+  const comments = Comments(article.article_id);
   return (
     <>
       <div className="article-position">
@@ -6,11 +13,24 @@ function SingleArticleCard(article) {
           <h1>{article.title}</h1>
           <img src={article.article_img_url} width="50%" />
           <h2>{article.topic}</h2>
-          <h4>{article.author}</h4>
+          <h4>Author: {article.author}</h4>
           <p>{article.body}</p>
-          <p>{article.created_at}</p>
+          <p className="article-date">{formatDate(article.created_at)}</p>
         </div>
       </div>
+      <div className="article-buttons">
+        <h2 className="comment-header">Comments</h2>
+      </div>
+      {comments.map((comment) => {
+        return (
+          <div className="comment-container">
+            <li key={comment.comment_id} className="comment-list">
+              <h2>{comment.author}</h2>
+              <h3>{comment.body}</h3>
+            </li>
+          </div>
+        );
+      })}
     </>
   );
 }
