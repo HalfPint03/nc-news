2b8ec0a26daf3fe1fa5bd14fcbc51fda5d46d760
Ashlyn Lovatt <ashlynlovatt@gmail.com>
2024-08-08 10:12:06 +0100

    task 7 votes

--- Components/Articles.jsx
+++ Components/Articles.jsx
@@ -1,15 +1,40 @@
 import { useEffect, useState } from "react";
-import fetchArticles from '../Components/Utils/fetchArticles'
+import fetchArticles from "../Components/Utils/fetchArticles";
 import ArticleCard from "./Utils/ArticleCard";
+import Lottie from 'react-lottie';
+import animationData from '../Components/Utils/lotties/Animation - 1723020635626.json';
+import React from 'react';
 
-function Articles(){
-    const [articles, setArticles] = useState([]);
+function Articles() {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchArticles().then((fetchedArticles) => {
-        setArticles(fetchedArticles);
-      });
+      setArticles(fetchedArticles);
+      setIsLoading(false);
+    });
   }, []);
+  if (isLoading) {
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    };
+  
+  return (
+    <div>
+      <Lottie 
+	    options={defaultOptions}
+        height={400}
+        width={400}
+      />
+    </div>
+  );
+  }
   return ArticleCard(articles);
 }
-export default Articles
+export default Articles;
--- Components/Utils/SingleArticleCard.jsx
+++ Components/Utils/SingleArticleCard.jsx
@@ -1,8 +1,6 @@
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import formatDate from "./formatDate";
 import Comments from "../Comments";
+import Votes from "../Votes";
 
 function SingleArticleCard(article) {
   const comments = Comments(article.article_id);
@@ -11,20 +9,19 @@ function SingleArticleCard(article) {
       <div className="article-position">
         <div className="single-article">
           <h1>{article.title}</h1>
-          <img src={article.article_img_url} width="50%" />
+          <img src={article.article_img_url} alt={`${article.title} image`} width="50%" />
           <h2>{article.topic}</h2>
           <h4>Author: {article.author}</h4>
           <p>{article.body}</p>
           <p className="article-date">{formatDate(article.created_at)}</p>
+          <Votes article={article}/>
         </div>
       </div>
-      <div className="article-buttons">
-        <h2 className="comment-header">Comments</h2>
-      </div>
+      <h2 className="comment-header">Comments</h2>
       {comments.map((comment) => {
         return (
-          <div className="comment-container">
-            <li key={comment.comment_id} className="comment-list">
+          <div className="comment-container" key={comment.comment_id}>
+            <li className="comment-list">
               <h2>{comment.author}</h2>
               <h3>{comment.body}</h3>
             </li>
